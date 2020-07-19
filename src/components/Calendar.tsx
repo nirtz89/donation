602627a82ecd22f3176c8ecc155213c67bec2687
Dean Geckt <degeckt@microsoft.com>
2020-07-19 16:06:50 +0300

    add calendat

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import './Calendar.scss';
+import React, { useState } from 'react';
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
 
-function Calendar() {
-  return (
-    <div className="Calendar">
-      Calendar
-    </div>
-  );
-}
+
+const Calendar = () => {
+    const [date, changeDate] = useState(new Date());
+
+    return (
+     <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <DatePicker
+              autoOk
+              orientation="landscape"
+              variant="static"
+              openTo="date"
+              value={date}
+              onChange={changeDate as any}
+        />
+      </MuiPickersUtilsProvider>
+    );
+  };
 
 export default Calendar;
