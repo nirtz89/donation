0964e9afff4cbb9714ec275475aa5e8f2f1ae4b2
MIDDLEEAST\docohen <docohen@microsoft.com>
2020-07-19 18:10:47 +0300

    add map

--- src/components/AppMap.tsx
+++ src/components/AppMap.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
 import './AppMap.scss';
+import React, { Component } from 'react'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+
+type State = {
+  lat: number,
+  lng: number,
+  zoom: number,
+}
+
+export class SimpleExample extends Component<{}, State> {
+  state = {
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 13,
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng]
+    return (
+      <Map center={position} zoom={this.state.zoom} style={{ width: '100%', height: '100%'}} >
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+    )
+  }
+}
 
 function AppMap() {
   return (
     <div className="AppMap">
-      Map
+      <SimpleExample />
     </div>
   );
 }
