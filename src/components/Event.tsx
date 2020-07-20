20d52c3bee4e6f1edae6ce59d5c28a836b6378c3
Nir Tzezana <nitzezan@microsoft.com>
2020-07-20 15:48:23 +0300

    added tooltip

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './Event.scss';
+import { Tooltip, Typography, withStyles } from '@material-ui/core';
 
 function Event(props) {
 
+  const HtmlTooltip = withStyles((theme) => ({
+    tooltip: {
+      maxWidth: 220,
+      fontSize: theme.typography.pxToRem(12)
+    },
+  }))(Tooltip);
+
   return (
-    <div className={`Event ${props.isCurrent ? 'current' : ''}`}>Event</div>
+    <HtmlTooltip placement="top" arrow
+    title={
+      <React.Fragment>
+        <Typography color="inherit">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, dolorem.</Typography>
+      </React.Fragment>
+    }>
+      <div className={`Event ${props.isCurrent ? 'current' : ''}`}>Event</div>
+    </HtmlTooltip>
   );
 }
 
