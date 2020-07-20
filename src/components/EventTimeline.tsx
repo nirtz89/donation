ee53a7e52276bf6de2f9d302c4ce543db3b68072
Nir Tzezana <nitzezan@microsoft.com>
2020-07-20 15:21:38 +0300

    timeline fixes

--- src/components/EventTimeline.tsx
+++ src/components/EventTimeline.tsx
@@ -4,9 +4,11 @@ import './EventTimeline.scss';
 
 function EventTimeline(props) {
 
+  // TODO: make scroll always stick to the end
+
   return (
     <div className="EventTimeline">
-        {[1,2,3,4].map((v)=><Event key={v} isCurrent={v===4} />)}
+        {[1,2,3,4,5,6,7,8].map((v)=><Event key={v} isCurrent={v===8} />)}
     </div>
   );
 }
