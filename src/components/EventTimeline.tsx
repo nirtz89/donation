e956822132150036fd7e802040876f803f491ed8
Nir Tzezana <nitzezan@microsoft.com>
2020-07-20 15:30:53 +0300

    event time line fixes and improvments

--- src/components/EventTimeline.tsx
+++ src/components/EventTimeline.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Event from './Event'
 import './EventTimeline.scss';
 
 function EventTimeline(props) {
 
-  // TODO: make scroll always stick to the end
+  const eventTimeLineRef = useRef<HTMLDivElement>(null);
 
+  eventTimeLineRef.current?.scrollTo(9999, 0);
   return (
-    <div className="EventTimeline">
+    <div className="EventTimeline" ref={eventTimeLineRef}>
         {[1,2,3,4,5,6,7,8].map((v)=><Event key={v} isCurrent={v===8} />)}
     </div>
   );
