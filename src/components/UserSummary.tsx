5c18f4d3cffb5db3042b3af9532a82c0ee171eff
Dean Geckt <degeckt@microsoft.com>
2020-07-30 11:00:04 +0300

    chart - change colors + text align

--- src/components/UserSummary.tsx
+++ src/components/UserSummary.tsx
@@ -38,20 +38,22 @@ const UserSummary = (props: IUserSummary) => {
             label: '# of Votes',
             data: counts,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 0.3)',
+                'rgba(5, 40, 235, 0.3)',
+                'rgba(255, 206, 86, 0.3)',
+                'rgba(1, 192, 192, 0.3)',
+                'rgba(153, 102, 255, 0.3)',
+                'rgba(10, 159, 64, 0.3)',
+                'rgba(255, 255, 1, 0.3)',
+                'rgba(200, 16, 254, 0.3)',
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
+                'rgba(5, 40, 235, 1)',
                 'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
+                'rgba(1, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
+                'rgba(10, 159, 64, 1)',
                 'rgba(255, 255, 1, 1)',
                 'rgba(200, 16, 254, 1)',
             ],
@@ -61,7 +63,7 @@ const UserSummary = (props: IUserSummary) => {
 
   return (
       <>
-        <div className="UserSummary">
+        <div className="UserSummary" style={{marginTop: 18, marginBottom: 65 ,fontSize: '22px', textAlign: 'center'}}>
             Thank you for completing the epidemiology inquiry
         </div>
         <Doughnut data={data} />
